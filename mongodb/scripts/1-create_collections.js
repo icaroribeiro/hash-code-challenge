b8187847a857cf5dc1c33b4e09fccce33b71603a
icaroribeiro <icaroribeiro@hotmail.com>
2020-03-26 23:17:15 -0300

    Developed a script file for creating collections

---
+++ mongodb/scripts/1-create_collections.js
@@ -0,0 +1,11 @@
+// Users.
+db.createCollection("users");
+
+// Products.
+db.createCollection("products");
+
+// Promotions.
+db.createCollection("promotions");
+
+// Discounted dates.
+db.createCollection("discountedDates");
