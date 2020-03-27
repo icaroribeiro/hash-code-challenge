7aded53060e4747f69abead118b28b71a3d918be
icaroribeiro <icaroribeiro@hotmail.com>
2020-03-27 07:15:11 -0300

    Developed a server to make resources available during the API requests

---
+++ back-end/go/internal/grpc/services/server/server.go
@@ -0,0 +1,17 @@
+package server
+
+import (
+    "github.com/icaroribeiro/hash-code-challenge/back-end/go/internal/mongodb"
+)
+
+// This structure is an abstraction of the server that allows to "attach" some resources in order to make them
+// available during the API calls. Here, it's used to store other structure that holds attributes to manage the data.
+type ServiceServer struct {
+	Datastore mongodb.Datastore
+}
+
+func CreateServiceServer(datastore mongodb.Datastore) ServiceServer {
+	return ServiceServer {
+		Datastore: datastore,
+	}
+}
