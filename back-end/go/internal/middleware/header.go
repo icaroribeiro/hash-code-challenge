bb0caae33e782a92e838147be6c2b7a58a413000
icaroribeiro <icaroribeiro@hotmail.com>
2020-03-26 23:56:19 -0300

    Developed the intermediate validations of API requests

---
+++ back-end/go/internal/middleware/header.go
@@ -0,0 +1,14 @@
+package middleware
+
+import (
+	"strings"
+)
+
+func CustomHeaderMatcher(key string) (string, bool) {
+	switch strings.ToUpper(key) {
+	case "X-USER-ID":
+		return key, true
+	default:
+		return key, false
+	}
+}
