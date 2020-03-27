f41c1814cb7166c456305d8c78089bff80003e92
icaroribeiro <icaroribeiro@hotmail.com>
2020-03-27 07:21:03 -0300

    Developed a file to use the dev env variables

---
+++ back-end/nodejs/env.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv').config({path: __dirname + '/.env'});
+
+module.exports = {
+    GRPC_SERVER_HOST: process.env.GRPC_SERVER_HOST,
+    GRPC_SERVER_PORT: process.env.GRPC_SERVER_PORT,
+    DB_USERNAME: process.env.DB_USERNAME,
+    DB_PASSWORD: process.env.DB_PASSWORD,    
+    DB_HOST: process.env.DB_HOST,    
+    DB_PORT: process.env.DB_PORT,    
+    DB_NAME: process.env.DB_NAME,
+};
