1ed2bb8cef21a9eacb0b26a0a4b2fe1323a893c1
icaroribeiro <icaroribeiro@hotmail.com>
2020-04-04 10:05:41 -0300

    Ajusted a few string variables to not include escape sequences

--- back-end/go/internal/mongodb_test/discounted-date_update_test.go
+++ back-end/go/internal/mongodb_test/discounted-date_update_test.go
@@ -63,19 +63,9 @@ func TestUpdateDiscountedDate(t *testing.T) {
         },
     }
 
-    body = fmt.Sprintf(`{
-        "title":"%s",
-        "description":"%s",
-        "discount_pct":%f,
-        "date":{
-            "year":%d,
-            "month":%d,
-            "day":%d
-        }
-    }`, discountedDate.Title, discountedDate.Description, discountedDate.DiscountPct,
+	body = fmt.Sprintf(`{"title":"%s","description":"%s","discount_pct":%f,"date":{"year":%d,"month":%d,"day":%d}}`,
+		discountedDate.Title, discountedDate.Description, discountedDate.DiscountPct,
         discountedDate.Date.Year, discountedDate.Date.Month, discountedDate.Date.Day)
-
-    body = utils.RemoveEscapeSequencesFromString(body, "\t", "\n")
 
     t.Logf("Update discounted date: %s", body)
 
