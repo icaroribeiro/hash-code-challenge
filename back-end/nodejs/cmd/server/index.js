0e297492f02ee9d3f97100ad75f530e4cf78e683
icaroribeiro <icaroribeiro@hotmail.com>
2020-04-13 21:45:35 -0300

    Developed a new approach to set up the environment variables

--- back-end/nodejs/cmd/server/index.js
+++ back-end/nodejs/cmd/server/index.js
@@ -1,18 +1,33 @@
 var envVariablesMap = require('../../env.js');
+var mongodb = require('../../internal/mongodb/index.js');
+var server = require('../../internal/grpc/services/server/index.js');
 
 var grpc = require('grpc');
 
-var ProductService = require('../../internal/grpc/services/product.js');
 var ProductServiceImpl = require('../../internal/grpc/services/impl/product_impl.js');
+var ProductService = require('../../internal/grpc/services/product.js');
+
+var dbConfig = {
+    Username: envVariablesMap.get('DB_USERNAME'),
+    Password: envVariablesMap.get('DB_PASSWORD'),
+    Host:     envVariablesMap.get('DB_HOST'),
+    Port:     envVariablesMap.get('DB_PORT'),
+    Name:     envVariablesMap.get('DB_NAME')
+};
+
+var datastore = mongodb.InitializeDB(dbConfig);
+
+var serviceServer = server.CreateServiceServer(datastore);
 
 var grpcAddress = envVariablesMap.get("GRPC_SERVER_HOST") + ":" + envVariablesMap.get("GRPC_SERVER_PORT");
 
 console.log('Starting the GRPC server connection on', grpcAddress);
 
-var server = new grpc.Server();
+var grpcServer = new grpc.Server();
 
-server.addService(ProductService.serviceDescriptor, ProductServiceImpl.serviceMap);
+var ProductServiceServer = ProductServiceImpl.NewProductServiceServer(serviceServer);
+grpcServer.addService(ProductService.serviceDescriptor, ProductServiceServer);
 
-server.bind(grpcAddress, grpc.ServerCredentials.createInsecure());
+grpcServer.bind(grpcAddress, grpc.ServerCredentials.createInsecure());
 
-server.start();
+grpcServer.start();
