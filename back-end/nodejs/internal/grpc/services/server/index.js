d05b12222337834bb47a4bca82cc5c58e1b39eb6
icaroribeiro <icaroribeiro@hotmail.com>
2020-04-15 08:27:00 -0300

    Updated the server to make resources available during the API requests

--- back-end/nodejs/internal/grpc/services/server/index.js
+++ back-end/nodejs/internal/grpc/services/server/index.js
@@ -1,4 +1,6 @@
 function CreateServiceServer(datastore) {
+    // This object is an abstraction of the server that allows to "attach" some resources in order to make them
+    // available during the API requests. Here, it's used to store other object that holds attributes to manage the data.
     return serviceServer = {
         Datastore: datastore,
     }
